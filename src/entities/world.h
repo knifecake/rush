599be8f6bcc096e5eddbe3aca0c3f82735de84f7
Elias Hernandis <eliashernandis@gmail.com>
2017-10-20 17:24:37 +0200

    world documentation

--- src/entities/world.h
+++ src/entities/world.h
@@ -23,14 +23,32 @@
 
 typedef struct _World World;
 
+/*
+ * Returns a new world object. Receives an array of pointers to all the
+ * available tiles and buildings. Returns NULL on failure.
+ */
 World *world_new(Tile **, Building **);
 
+/*
+ * Destroys a world, freeing all associated objects.
+ *
+ * Does nothing on invalid input.
+ */
 void world_destroy(World *);
 
+/*
+ * Constructs the internal map for the world, chosing from the available tiles.
+ */
 World *world_load_map(World *);
 
+/*
+ * Lets player perform actions.
+ */
 World *world_player_turn(World *, Player *);
 
+/*
+ * Updates the world between turns of the player.
+ */
 World *world_ai_turn();
 
 
