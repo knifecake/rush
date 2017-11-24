629b4ac3af5acd654ec03744bbd70e7f5b9a8b16
Elias Hernandis <eliashernandis@gmail.com>
2017-11-24 16:41:16 +0100

    fixing some shit

--- src/lib/sprite.h
+++ src/lib/sprite.h
@@ -53,8 +53,4 @@ int sprite_get_h(Sprite *s);
  * Returns the width of the given sprite, PINT_ERROR on error.
  */
 int sprite_get_w(Sprite *s);
-<<<<<<< HEAD
-=======
-
 #endif
->>>>>>> c88e926a4571709dcb784c05ac38ba49dfb5bf51
